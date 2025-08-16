ba789eea529caebb78da28303c320491a572e5d8
Ya <gx09950113@gmail.com>
2025-08-17 02:51:28 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("add-herb").onclick = () => {
+  alert("你點了添加藥材！");
+};
+
+document.getElementById("start-brew").onclick = () => {
+  alert("煉丹開始了！");
+};
+
+document.getElementById("reset").onclick = () => {
+  alert("煉丹爐已重置！");
+};
